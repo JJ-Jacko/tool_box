26fbc494e9cef3fa9903328a91653bd32e9cc7f9
Jacko <l2982211930@163.com>
2025-12-23 19:21:36 +0800

    refactor(libs/path/get_file_hash): 获取文件的哈希值

--- src/video_tools/libs/path.py
+++ src/video_tools/libs/path.py
@@ -1,3 +1,4 @@
+import hashlib
 from pathlib import Path
 from typing import Generator
 from typing import Tuple
@@ -38,4 +39,29 @@ def iter_dir_file(
                 continue
         
         yield sub_p
-        
+
+
+def get_file_hash(
+        file_path: Path,
+        algorithm: str = "MD5"
+):
+    """获取文件的哈希值
+
+    Args:
+        file_path: 文件的的路径
+        algorithm: 算法类型 默认 `MD5`
+    """
+    
+    if not file_path.is_file():
+        raise ValueError("传入的路径必须为文件")
+    
+    hash_obj = hashlib.new(algorithm)
+
+    # 读取文件块并更新哈希对象
+    with file_path.open("rb") as f:
+        while chunk := f.read(4096): # 4096 字节为一个块
+            hash_obj.update(chunk)
+
+    
+    # 获取十六进制形式的哈希值
+    return hash_obj.hexdigest()
