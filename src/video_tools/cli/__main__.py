f805e568b70da3c5374c89b1a320dac15a51d50e
Jacko <l2982211930@163.com>
2026-08-03 19:39:28 +0800

    refactor(cli): Standardize name.

--- src/video_tools/cli/__main__.py
+++ src/video_tools/cli/__main__.py
@@ -2,15 +2,15 @@ from . import compressed_files_extract
 from . import files_duplicate
 from . import files_rename
 from . import files_summary
-from . import image_color_reverse
-from . import image_to_pdf
-from . import media_encoding
+from . import images_color_reverse
+from . import images_to_pdf
+from . import videos_encoding
 
 
 # compressed_files_extract.run()
 # files_duplicate.run()
 # files_rename.run()
 # files_summary.run()
-# image_color_reverse.run()
-# image_to_pdf.run()
-# media_encoding.run()
+# images_color_reverse.run()
+# images_to_pdf.run()
+# videos_encoding.run()
