297b7b1d57e1eb3bf4b3a248d45285474adc6908
Jacko <l2982211930@163.com>
2026-08-03 15:10:21 +0800

    feat(log.get_logger): Support for muti threads name.

--- src/video_tools/libs/log.py
+++ src/video_tools/libs/log.py
@@ -1,29 +1,53 @@
-"""日志库"""
+"""Logging utils"""
+
 import logging
 import logging.handlers
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime as DateTime
 
 
-formatter = logging.Formatter(
-    fmt="%(asctime)s - %(levelname)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
-)
+__all__ = [
+    "get_logger"
+]
 
 
-def get_logger(name: str):
-    """获取 logger
+datefmt="%Y-%m-%d %H:%M:%S"
+fmt_map = {
+    "single_thread": "%(asctime)s - %(levelname)s - %(message)s",
+    "multi_thread": "%(asctime)s - [%(threadName)s] - %(levelname)s - %(message)s"
+}
 
-    Args:
-        name (str): 日志名字
 
-    Returns:
-        logger (Logger): 日志
+def get_logger(
+        name: str,
+        console: bool = True,
+        file: bool = True,
+        multi_thread: bool = False
+):
     """
+    Args:
+        console:
+            output to console.
+            输出到控制台
+        file:
+            output to file.
+            输出到文件
+    """
+    
+    if multi_thread:
+        formatter = logging.Formatter(
+            datefmt=datefmt,
+            fmt=fmt_map.get("multi_thread")
+        )
+    else:
+        formatter = logging.Formatter(
+            datefmt=datefmt,
+            fmt=fmt_map.get("single_thread")
+        )
     
     log_dir = Path("logs")
     log_dir.mkdir(parents=True, exist_ok=True)
-    log_file = log_dir / f"{name}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log"
+    log_file = log_dir / f"{name}_{DateTime.now().strftime("%Y-%m-%d_%H-%M-%S")}.log"
     file_handler = logging.handlers.RotatingFileHandler(log_file)
     file_handler.setLevel(logging.INFO)
     file_handler.setFormatter(formatter)
@@ -34,7 +58,9 @@ def get_logger(name: str):
 
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    if console:
+        logger.addHandler(console_handler)
+    if file:
+        logger.addHandler(file_handler)
     
     return logger
