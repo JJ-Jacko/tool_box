5f2175e113c96ada619deaf04412005e6487807c
Jacko <l2982211930@163.com>
2026-08-03 19:28:43 +0800

    refactor(cli.files_sum -> files_summary)

--- src/video_tools/cli/__main__.py
+++ src/video_tools/cli/__main__.py
@@ -1,7 +1,7 @@
 from . import compressed_files_extract
 from . import files_duplicate
 from . import files_rename
-from . import files_sum
+from . import files_summary
 from . import image_color_reverse
 from . import image_to_pdf
 from . import media_encoding
@@ -10,7 +10,7 @@ from . import media_encoding
 # compressed_files_extract.run()
 # files_duplicate.run()
 # files_rename.run()
-# files_sum.run()
+# files_summary.run()
 # image_color_reverse.run()
 # image_to_pdf.run()
 # media_encoding.run()
